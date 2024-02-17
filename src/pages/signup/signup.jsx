b3f46551aabd67e9d3b3235ae0075d87059864d2
Anas Waleed Tahir <116335388+ANAS-TAAHIR@users.noreply.github.com>
2024-02-17 21:59:41 +0500

    Update signup.jsx

--- src/pages/signup/signup.jsx
+++ src/pages/signup/signup.jsx
@@ -1,20 +1,39 @@
-import React from "react";
-import { useState } from 'react';
+import React, { useState } from 'react';
 import { useFirebase } from "../../context/Firebase";
 
 const SignUp = () => {
-    const firebase = useFirebase();
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    return (
-        <div>
-            <h2>Sign Up</h2>
-            <label>Email</label>
-            <input onChange={(e) => setEmail(e.target.value)} type="email" placeholder='Email'/>
-            <label>Password</label>
-            <input onChange={(e) => setPassword(e.target.value)} type="password" placeholder='Password' />
-            <button onClick={() => firebase.signUpUserwithEmailAndPassword(email , password)}>Sign Up</button>
-        </div>
-    );
-}
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const firebase = useFirebase();
+
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Add your sign-in logic here
+    firebase.signUpUserwithEmailAndPassword(email, password);
+  };
+
+  return (
+    <div>
+      <h2>Sign Up</h2>
+      <form onSubmit={handleSubmit}>
+          <label>Email:</label>
+          <input type="email" placeholder='Email' value={email}
+          onChange={handleEmailChange} />
+          <label>Password:</label>
+          <input type="password" placeholder='Password' value={password}
+          onChange={handlePasswordChange} />
+        <button type="submit">Sign Up</button>
+      </form>
+    </div>
+  );
+};
+
 export default SignUp;
