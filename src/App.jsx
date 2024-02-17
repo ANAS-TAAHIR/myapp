51c017f3cf34d72ffe79a7e8c95e8526324bd1f4
anas-waleed-tahir <anastahir707070@gmail.com>
2024-02-17 21:53:02 +0500

    change in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
 import './App.css'
-import { useFirebase } from './context/Firebase'
-import { useState } from 'react';
 import SignUp from './pages/signup/signup'
 import SignIn from './pages/signin/signin'
 
